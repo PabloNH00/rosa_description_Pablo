6cd8e1fd2af978e21fa283ee524904356c907fab
mhernando <miguel.hernando@upm.es>
2023-11-08 09:23:00 +0100

    uodate

--- launch/rosa_gazebo_launch.py
+++ launch/rosa_gazebo_launch.py
@@ -1,54 +1,52 @@
 import os
-from ament_index_python.packages import get_package_prefix
+import xacro
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-import xacro
 
-def xacro_2_urdf_with_absolute_refs(pkg, xacro_model):
-    share_dir =  get_package_share_directory(pkg)
-    xacro_file = os.path.join(share_dir, xacro_model)
-    robot_description_config = xacro.process_file(xacro_file)
-    urdf_content = robot_description_config.toprettyxml().replace(f'package://{pkg}',share_dir)
-    urdf_file = os.path.join(share_dir,f'{xacro_model}.urdf')
-    with open(urdf_file, 'w') as file:
-        file.write(urdf_content)
-    return urdf_file, urdf_content
-    
+
+  
 def generate_launch_description():
-  share_dir =  get_package_share_directory('rosa_description')
-  world_file = os.path.join(share_dir,'models','worlds','pal_office.world')
-  urdf_file, urdf_content = xacro_2_urdf_with_absolute_refs('rosa_description','models/rosa/rosa.xacro')
+
   use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+ 
 
-  return LaunchDescription([
+  share_dir =  get_package_share_directory('rosa_description')
+  world_file = os.path.join(share_dir,'worlds','pal_office.world')
+  model_file = os.path.join(share_dir, 'description', 'rosa','rosa.xacro')
+  urdf_content = xacro.process_file(model_file).toprettyxml()
+  
 
-        DeclareLaunchArgument(
+
+  ld = LaunchDescription()
+  ld.add_action(DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-
-        ExecuteProcess(
-            cmd=['gazebo', world_file ,'--verbose', '-s', 'libgazebo_ros_factory.so'],
-            output='screen'),
-        Node(
+            description='Use simulation (Gazebo) clock if true'))
+            
+  ld.add_action(ExecuteProcess(
+            cmd=['gazebo', world_file , '-s', 'libgazebo_ros_factory.so'],
+            output='screen'))
+  #'--verbose' add this as parameter of gazebo command to see more info          
+  ld.add_action(Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
             name='urdf_spawner',
             output='screen',
-            arguments=["-topic", "/robot_description", "-entity", "rosa"]),
-
-        Node(
+            arguments=["-topic", "/robot_description", "-entity", "rosa"]) )
+            
+  ld.add_action(Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            output='screen'))    
+                  
+  ld.add_action(Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[{'robot_description':urdf_content, 'use_sim_time': use_sim_time }],
-            arguments = [urdf_file]),
-            
-
- 
- 
-  ])
+            parameters=[{'robot_description':urdf_content, 'use_sim_time': use_sim_time }])) 
+                         
+  return ld
